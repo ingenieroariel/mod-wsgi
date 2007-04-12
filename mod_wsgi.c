54807ee292bd995d3aaca072d2c5bab01db8955b
Graham.Dumpleton <Graham.Dumpleton@a261f285-392b-0410-9c51-7d95846f2947>
2007-04-12 11:50:09 +0000

    Collapse duplicate slashes in script name calculated for application group.

--- mod_wsgi.c
+++ mod_wsgi.c
@@ -346,14 +346,26 @@ typedef struct {
 
 static const char *wsgi_script_name(request_rec *r)
 {
+    const char *script_name = NULL;
     int path_info_start = 0;
 
-    if (!r->path_info || !*r->path_info)
-        return r->uri;
+    if (!r->path_info || !*r->path_info) {
+        script_name = r->uri;
+    }
+    else {
+        path_info_start = ap_find_path_info(r->uri, r->path_info);
 
-    path_info_start = ap_find_path_info(r->uri, r->path_info);
+        script_name = apr_pstrndup(r->pool, r->uri, path_info_start);
+    }
 
-    return apr_pstrndup(r->pool, r->uri, path_info_start);
+    if (*script_name) {
+        while (*script_name && (*(script_name+1) == '/'))
+            script_name++;
+        script_name = apr_pstrdup(r->pool, script_name);
+        ap_no2slash((char*)script_name);
+    }
+
+    return script_name;
 }
 
 static const char *wsgi_process_name(request_rec *r, const char *s)
