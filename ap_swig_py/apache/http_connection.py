8108e9390e4df0a1fda8269aa4ff093b7adbf10e
Graham.Dumpleton <Graham.Dumpleton@a261f285-392b-0410-9c51-7d95846f2947>
2008-04-11 04:52:40 +0000

    Move ap_swig_py package containing SWIG bindings for Apache out of trunk and into sandbox.

--- ap_swig_py/apache/http_connection.py
+++
@@ -1,8 +0,0 @@
-import apache
-
-if apache.version == (2, 2):
-    from apache22.http_connection import *
-elif apache.version == (2, 0):
-    from apache20.http_connection import *
-else:
-    raise RuntimeError('Apache version not supported.')
--- ap_swig_py/apache/mod_auth.py
+++
@@ -1,6 +0,0 @@
-import apache
-
-if apache.version == (2, 2):
-    from apache22.mod_auth import *
-else:
-    raise RuntimeError('Apache version not supported.')
